908559e4298de603a8a6bb7914dd626cafd5d65d
heisenburgers <zkdaoorganization@gmail.com>
2023-10-25 20:10:51 +0400

    initial states & setup, navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,112 +1,101 @@
+"use client"
+
 import Image from 'next/image'
+import key2 from "../public/images/key2.png"
+import key from "../public/images/key.png"
+import triangle from '../public/images/triangle.png'
+import { useState } from 'react';
 
 export default function Home() {
+  const [isKeySelected, setIsKeySelected] = useState(false);
+
+  // Menu
+  const [selectedGear, setSelectedGear] = useState("P");
+  const [selectorMargin, setSelectorMargin] = useState("0");
+  const gears = ["P", "R", "N", "D", "2", "1"];
+  const pointerPositions = ["0px", "40px", "80px", "120px", "160px", "200px"]
+
+  const selectedCamaro = () => {
+    setIsKeySelected(true)
+  }
+
+  const selectedMustang = () => {
+    setIsKeySelected(true)
+    
+  }
+
+  const test = () => {
+    setIsKeySelected(false)
+  }
+  //mt-[${selectorMargin}px]
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="flex bg-radial-gradient min-h-screen justify-center items-center">
+
+      <nav className="fixed right-20 py-4 top-50 flex justify-center items-center z-50">
+        <div className="flex flex-col gap-4">
+          {gears.map((gear, index) => (
+            <a 
+              key={index}
+              onClick={() => {setSelectedGear(gear); setSelectorMargin(pointerPositions[index])}}
+              href={`#section${index + 1}`}
+              className={`pl-3 pr-3 text-white hover:text-gray-400 cursor-pointer ${selectedGear === gear ? "text-speedometer-red" : ""} transition`}
+            >
+              {gear}
+            </a>
+          ))}
         </div>
-      </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+        <div
+          className={`flex flex-col gap-[26px] transition ease-in-out transform-gpu duration-300`}
+          style={{ transform: `translateY(${selectorMargin})` }}
+          >
+            {gears.map((gear, index) => (
+              <div
+                key={index}
+                className={`pr-3 ${index === 0 ? "opacity-100" : "opacity-0"}`}
+              >
+                <Image src={triangle} width={12} alt="selector" />
+              </div>
+            ))}
+        </div>
+        
+      </nav>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+      <div className="flex w-full h-full justify-center items-center">
+        
+        {/* This is the parent div that encompasses the two sections */}
+        <div className="flex flex-col w-full h-screen flex justify-center items-center">
+          
+          {/* First section */}
+          <div className="flex flex-col w-full h-full justify-center items-center text-white text-4xl font-librebodoni font-bold tracking-wider">
+            <span>HI, I'M LUKA </span>
+            <span>SENIOR FRONT-END ENGINEER</span>
+          </div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+          {/* Second section */}
+          <div className="flex w-full h-full justify-center items-center">
+            <div className={`flex w-full ${isKeySelected ? "justify-center" : "justify-evenly"} items-center`}>
+              {isKeySelected ?
+              (
+                <Image src={key} width={128} height={128} alt="Camaro" onClick={test} />
+              )
+              :
+              (
+                <>
+                  <div onClick={selectedCamaro}>
+                    <Image src={key2} width={128} height={128} alt="Camaro" />
+                  </div>
+                  <div onClick={selectedMustang}>
+                    <Image src={key2} width={128} height={128} alt="Camaro" />
+                  </div>
+                </>
+              )}
+              
+            </div>
+          </div>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        </div>
+        
       </div>
     </main>
   )
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,9 +9,36 @@ const config: Config = {
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'radial-gradient': 'radial-gradient(circle farthest-side at 50% 50%, #000000 0%, #020202 100%)',
+      },
+      colors: {
+        'background': '#020202',
+        'dark-off-black': '#1A1A1A',
+        'light-gray': '#B0B0B0',
+        'wood-brown': '#8A4818',
+        'speedometer-red': '#E95239',
+        'soft-blue': '#E0E9F6',
+        'white': '#FDFDFD',
+      },
+      borderRadius: {
+        '5': '5px',
+        '10': '10px',
+        '15': '15px',
+        '20': '20px',
+      },
+      fontSize: {
+        'xs': '12px',
+        'sm': '14px',
+        'base': '16px',
+        'lg': '20px',
+        'xl': '24px',
+        '2xl': '32px',
+        '3xl': '40px',
+        '4xl': '48px',
+      },
+      fontFamily: {
+        'din': ['DIN', 'sans-serif'],
+        'librebodoni': ['LibreBodoniv2002', 'serif']
       },
     },
   },
